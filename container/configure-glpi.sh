accf7eb7dfdbaf819568adf423176b6d774619b4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-05-08 17:55:27 +0200

    fix: update supervisord configuration for GLPI and adjust script paths

--- container/configure-glpi.sh
+++ container/configure-glpi.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# # Wait for MySQL to be ready
-# echo "Waiting for MySQL to start..."
-# while ! mysqladmin ping -h localhost --silent; do
-#     sleep 2
-# done
-# echo "MySQL is up."
-
-# # Wait for Apache to be ready
-# echo "Waiting for Apache to start..."
-# while ! curl -s http://localhost > /dev/null; do
-#     sleep 2
-# done
-# echo "Apache is up."
+ # Wait for MySQL to be ready
+ echo "Waiting for MySQL to start..."
+ while ! mysqladmin ping -h localhost --silent; do
+     sleep 2
+ done
+ echo "MySQL is up."
+ # Wait for Apache to be ready
+ echo "Waiting for Apache to start..."
+ while ! curl -s http://localhost > /dev/null; do
+     sleep 2
+ done
+ echo "Apache is up."
 
 # Proceed with GLPI configuration
 echo "Starting GLPI configuration..."
 
-if -f /var/www/glpi/install/install.php; then
+if [  -f "/app/install/install.php" ]; then
     echo "GLPI installation script found. Proceeding with installation..."
     # Run the GLPI installation script
     _user=${MYSQL_USER_NAME:?}
@@ -29,7 +28,7 @@ if -f /var/www/glpi/install/install.php; then
     mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql  mysql
     mysql -uroot -e "GRANT SELECT ON mysql.time_zone_name TO 'glpi'"
     echo "=> remove install.php"
-    rm -rf /var/www/glpi/install/install.php
+    rm -rf /app/install/install.php
 else
     echo "GLPI installation script not found. upgrading installation."
     su - www-data -s /bin/bash -c "/usr/bin/php  /app/bin/console glpi:maintenance:enable"
